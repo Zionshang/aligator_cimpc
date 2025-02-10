ce72f2718d38bf92d06976adad9f4571ccee4f08
zishangji <zishangji@qq.com>
2025-02-10 15:11:48 +0800

    添加gitignore

--- main.cpp
+++ main.cpp
@@ -7,22 +7,26 @@
 #include <aligator/modelling/dynamics/integrator-semi-euler.hpp>
 #include <aligator/solvers/proxddp/solver-proxddp.hpp>
 #include <aligator/modelling/autodiff/finite-difference.hpp>
+#include <aligator/modelling/dynamics/integrator-euler.hpp>
 #include <fstream>
 
 #include "CompliantContactFwdDynamics.hpp"
+#include "logger.hpp"
 
+namespace pin = pinocchio;
 using aligator::context::TrajOptProblem;
 using StageModel = aligator::StageModelTpl<double>;
 using IntegratorSemiImplEuler = aligator::dynamics::IntegratorSemiImplEulerTpl<double>;
+using IntegratorEuler = aligator::dynamics::IntegratorEulerTpl<double>;
 using CostStack = aligator::CostStackTpl<double>;
 using QuadraticStateCost = aligator::QuadraticStateCostTpl<double>;
 using QuadraticControlCost = aligator::QuadraticControlCostTpl<double>;
 using DynamicsFiniteDifference = aligator::autodiff::DynamicsFiniteDifferenceHelper<double>;
 
-TrajOptProblem createTrajOptProblem(const CompliantContactFwdDynamics &dynamics,
-                                    int nsteps, double timestep,
-                                    const VectorXd &x_ref_start, const VectorXd &x_ref_end,
-                                    const VectorXd &x0, const VectorXd &u0)
+std::shared_ptr<TrajOptProblem> createTrajOptProblem(const CompliantContactFwdDynamics &dynamics,
+                                                     int nsteps, double timestep,
+                                                     const std::vector<VectorXd> &x_ref,
+                                                     const VectorXd &x0, const VectorXd &u0)
 {
     const auto space = dynamics.space();
     const int nu = dynamics.nu(); // Number of control inputs
@@ -31,7 +35,7 @@ TrajOptProblem createTrajOptProblem(const CompliantContactFwdDynamics &dynamics,
     // Define stage state weights
     VectorXd w_pos_diag = VectorXd::Zero(space.getModel().nv);
     VectorXd w_vel_diag = VectorXd::Zero(space.getModel().nv);
-    w_pos_diag << 10, 10, 50, // linear part
+    w_pos_diag << 10, 10, 10, // linear part
         1, 1, 1,              // angular part
         0, 0, 0,              // leg part
         0, 0, 0,
@@ -48,7 +52,7 @@ TrajOptProblem createTrajOptProblem(const CompliantContactFwdDynamics &dynamics,
     MatrixXd w_x = w_x_diag.asDiagonal();
 
     // Define terminal state weights
-    w_pos_diag << 10, 10, 50,
+    w_pos_diag << 10, 10, 10,
         10, 10, 10,
         1, 1, 1,
         1, 1, 1,
@@ -65,30 +69,41 @@ TrajOptProblem createTrajOptProblem(const CompliantContactFwdDynamics &dynamics,
 
     // Define input state weights
     MatrixXd w_u = MatrixXd::Identity(nu, nu);
-    w_u.diagonal().array() = 0.01;
+    w_u.diagonal().array() = 1e-6;
 
     IntegratorSemiImplEuler discrete_dyn = IntegratorSemiImplEuler(dynamics, timestep);
+    // IntegratorEuler discrete_dyn = IntegratorEuler(dynamics, timestep);
     DynamicsFiniteDifference finite_diff_dyn(space, discrete_dyn, 1e-6);
 
     std::vector<xyz::polymorphic<StageModel>> stage_models;
     for (size_t i = 0; i < nsteps; i++)
     {
-        // 计算当前时刻的参考状态（线性插值）
-        double alpha = static_cast<double>(i) / (nsteps - 1);
-        VectorXd x_ref = (1.0 - alpha) * x_ref_start + alpha * x_ref_end;
-        std::cout << "x_ref: " << x_ref.transpose() << std::endl;
         auto rcost = CostStack(space, nu);
-        rcost.addCost("quad_state", QuadraticStateCost(space, nu, x_ref, w_x));
-        rcost.addCost("quad_control", QuadraticControlCost(space, u0, w_u));
+        rcost.addCost("state_cost", QuadraticStateCost(space, nu, x_ref[i], w_x));
+        rcost.addCost("control_cost", QuadraticControlCost(space, u0, w_u));
 
         // stage_models.push_back(StageModel(rcost, discrete_dyn));
         stage_models.push_back(StageModel(rcost, finite_diff_dyn));
     }
 
-    auto term_cost = QuadraticStateCost(space, nu, x_ref_end, w_x_term);
-    TrajOptProblem problem(x0, stage_models, term_cost);
-    std::cout << "problem.numSteps() " << problem.numSteps() << std::endl;
-    return problem;
+    auto term_cost = CostStack(space, nu);
+    term_cost.addCost("term_state_cost", QuadraticStateCost(space, nu, x_ref.back(), w_x_term));
+
+    return std::make_shared<TrajOptProblem>(x0, stage_models, term_cost);
+}
+
+void updateStateReferences(std::shared_ptr<TrajOptProblem> problem,
+                           const std::vector<VectorXd> &x_ref)
+{
+    for (size_t i = 0; i < problem->numSteps(); i++)
+    {
+        CostStack *cs = dynamic_cast<CostStack *>(&*problem->stages_[i]->cost_);
+        QuadraticStateCost *qsc = cs->getComponent<QuadraticStateCost>("state_cost");
+        qsc->setTarget(x_ref[i]);
+    }
+    CostStack *cs = dynamic_cast<CostStack *>(&*problem->term_cost_);
+    QuadraticStateCost *qsc = cs->getComponent<QuadraticStateCost>("term_state_cost");
+    qsc->setTarget(x_ref.back());
 }
 
 int main(int argc, char const *argv[])
@@ -113,27 +128,27 @@ int main(int argc, char const *argv[])
     double timestep = 0.05;
     const int nq = rmodel.nq;
     const int nv = rmodel.nv;
-    VectorXd x_ref_start = VectorXd::Zero(nq + nv);
-    x_ref_start.head(nq) << 0.0, 0.0, 0.29,
-        0.0, 0.0, 0.0, 1.0,
-        0.0, -0.8, 1.6,
-        0.0, -0.8, 1.6,
-        0.0, -0.8, 1.6,
-        0.0, -0.8, 1.6;
-    VectorXd x_ref_end = VectorXd::Zero(nq + nv);
-    x_ref_end.head(nq) << 0.5, 0.0, 0.29,
-        0.0, 0.0, 0.0, 1.0,
-        0.0, -0.8, 1.6,
-        0.0, -0.8, 1.6,
-        0.0, -0.8, 1.6,
-        0.0, -0.8, 1.6;
-    VectorXd x0 = x_ref_start;
-    VectorXd u0 = VectorXd::Zero(dynamics.nu());
 
-    auto problem = createTrajOptProblem(dynamics,
-                                        nsteps, timestep,
-                                        x_ref_start, x_ref_end,
-                                        x0, u0);
+    VectorXd x0 = VectorXd::Zero(nq + nv);
+    VectorXd u0 = VectorXd::Zero(dynamics.nu());
+    x0.head(nq) << 0.0, 0.0, 0.16,
+        0.0, 0.0, 0.0, 1.0,
+        0.0, -1.3, 2.4,
+        0.0, -1.3, 2.4,
+        0.0, -1.3, 2.4,
+        0.0, -1.3, 2.4;
+
+    VectorXd x_nom = VectorXd::Zero(nq + nv);
+    x_nom.head(nq) << 0.0, 0.0, 0.16,
+        0.0, 0.0, 0.0, 1.0,
+        0.0, -1.3, 2.4,
+        0.0, -1.3, 2.4,
+        0.0, -1.3, 2.4,
+        0.0, -1.3, 2.4;
+
+    std::vector<VectorXd> x_ref(nsteps, x_nom);
+
+    auto problem = createTrajOptProblem(dynamics, nsteps, timestep, x_ref, x0, u0);
 
     double tol = 1e-4;
     int max_iters = 100;
@@ -146,48 +161,38 @@ int main(int argc, char const *argv[])
     u_guess.assign(nsteps, u0);
 
     solver.rollout_type_ = aligator::RolloutType::LINEAR;
-    solver.sa_strategy_ = aligator::StepAcceptanceStrategy::FILTER;
-    solver.filter_.beta_ = 1e-5;
+    // solver.sa_strategy_ = aligator::StepAcceptanceStrategy::FILTER;
+    // solver.filter_.beta_ = 1e-5;
     solver.force_initial_condition_ = true;
-    solver.reg_min = 1e-6;
+    // solver.reg_min = 1e-6;
     solver.linear_solver_choice = aligator::LQSolverChoice::PARALLEL;
-    solver.setNumThreads(4); // call before setup()
-    solver.setup(problem);
+    solver.setNumThreads(8); // call before setup()
+    solver.setup(*problem);
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    solver.run(problem, x_guess, u_guess);
+    // first solve
+    solver.run(*problem, x_guess, u_guess);
+    x_guess = solver.results_.xs;
+    u_guess = solver.results_.us;
 
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    fmt::print("Total execution time: {} ms\n", duration.count());
-
-    auto &res = solver.results_;
-    fmt::print("Results: {}\n", res);
-
-    // 保存优化结果到CSV文件
-    std::ofstream outFile("trajectory_results.csv");
-    if (outFile.is_open())
+    // warm start
+    std::cout << "----------------------warm start----------------------" << std::endl;
+    for (int i = 0; i < nsteps; i++)
     {
-        // 写入数据
-        for (size_t i = 0; i < solver.results_.xs.size(); i++)
-        {
-            VectorXd state = solver.results_.xs[i].head(nq);
-            for (int j = 0; j < nq; ++j)
-            {
-                outFile << state[j];
-                if (j < nq - 1)
-                    outFile << ",";
-            }
-            outFile << "\n";
-        }
-        outFile.close();
-        std::cout << "Results saved to trajectory_results.csv" << std::endl;
+        x_ref[i].head(nq) << 0.0, 0.0, 0.29,
+            0.0, 0.0, 0.0, 1.0,
+            0.0, -0.8, 1.6,
+            0.0, -0.8, 1.6,
+            0.0, -0.8, 1.6,
+            0.0, -0.8, 1.6;
     }
-    else
-    {
-        std::cerr << "Unable to open file for writing" << std::endl;
-    }
+    updateStateReferences(problem, x_ref);
+    solver.max_iters = 1;
+    solver.run(*problem, x_guess, u_guess);
+
+    std::vector<VectorXd> x_result = solver.results_.xs;
+    saveVectorsToCsv("aligator_cimpc.csv", x_result);
 
     return 0;
 }
