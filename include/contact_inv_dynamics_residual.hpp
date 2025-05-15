0b4f391c7caef63dd6d60f56f4102461e978f382
Zishang <zishangji@qq.com>
2025-05-15 18:01:17 +0800

    完善逆动力学类，并通过cppad求导

--- include/contact_inv_dynamics_residual.hpp
+++ include/contact_inv_dynamics_residual.hpp
@@ -23,7 +23,7 @@ struct ContactInvDynamicsResidual : StageFunction
     void computeJacobians(const ConstVectorRef &x, const ConstVectorRef &u,
                           StageFunctionData &data) const;
 
-    shared_ptr<StageFunctionData> createData() const;
+    std::shared_ptr<StageFunctionData> createData() const;
 
     pinocchio::Model model_; // 存储着pinocchio模型
     MatrixXd actuation_matrix_;
@@ -32,11 +32,11 @@ struct ContactInvDynamicsResidual : StageFunction
 
 struct ContactInvDynamicsResidualData : StageFunctionData
 {
-    ContactInvDynamicsResidualData(const GravityCompensationResidualTpl &resdl);
+    ContactInvDynamicsResidualData(const ContactInvDynamicsResidual &resdl);
 
     VectorXd tau_;
     pinocchio::Data data_;
-    MatrixXd dtau_du_;
+    MatrixXd dtau_dtaua_; // 广义力矩对驱动力矩的偏导
     CppAD::ADFun<double> ad_inv_dynamics_;
     std::vector<Vector3d> contact_forces_; // 仅仅用于记录数据便于debug
 };
--- src/contact_inv_dynamics_residual.cpp
+++ src/contact_inv_dynamics_residual.cpp
@@ -1,10 +1,11 @@
 #include "contact_inv_dynamics_residual.hpp"
 
 #include <pinocchio/algorithm/rnea.hpp>
+#include <pinocchio/algorithm/joint-configuration.hpp>
 
 ContactInvDynamicsResidual::ContactInvDynamicsResidual(int ndx, const Model &model, MatrixXd actuation,
                                                        ContactParams<double> contact_params)
-    : StageFunction(ndx, actuation.cols(), model.nv),
+    : StageFunction(ndx, model.nv + actuation.cols(), model.nv),
       model_(model), actuation_matrix_(actuation), contact_params_(contact_params)
 {
     const int nv = model_.nv;
@@ -18,15 +19,17 @@ ContactInvDynamicsResidual::ContactInvDynamicsResidual(int ndx, const Model &mod
 }
 
 void ContactInvDynamicsResidual::evaluate(const ConstVectorRef &x, const ConstVectorRef &u,
-                                          StageFunctionData &data)
+                                          StageFunctionData &data) const
 {
     ContactInvDynamicsResidualData &d = static_cast<ContactInvDynamicsResidualData &>(data);
 
-    const int nq = model.nq;
-    const int nv = model.nv;
+    const int nq = model_.nq;
+    const int nv = model_.nv;
+    const int nu = actuation_matrix_.cols();
     const auto &q = x.head(nq);
     const auto &v = x.segment(nq, nv);
-    const auto &a = u;
+    const auto &a = u.head(nv);
+    const auto &tau = actuation_matrix_ * u.tail(nu);
 
     // 计算接触力
     pinocchio::forwardKinematics(model_, d.data_, q, v);
@@ -37,25 +40,73 @@ void ContactInvDynamicsResidual::evaluate(const ConstVectorRef &x, const ConstVe
     // 逆动力学计算
     d.tau_ = pinocchio::rnea(model_, d.data_, q, v, a, f_ext);
 
-    data.value_ = d.tau_ 
+    data.value_ = d.tau_ - tau;
 }
 
 void ContactInvDynamicsResidual::computeJacobians(const ConstVectorRef &x, const ConstVectorRef &u,
-                                                  StageFunctionData &data)
+                                                  StageFunctionData &data) const
 {
+    ContactInvDynamicsResidualData &d = static_cast<ContactInvDynamicsResidualData &>(data);
+    const int nq = model_.nq;
+    const int nv = model_.nv;
+    const int nu = actuation_matrix_.cols();
+
+    Eigen::VectorXd X(nq + nv + nv + nv); // q, v, a, dq
+    X << x, u.head(nv), Eigen::VectorXd::Zero(nv);
+
+    Eigen::VectorXd dtau_dx_vec = d.ad_inv_dynamics_.Jacobian(X);
+    Eigen::MatrixXd dtau_dx = dtau_dx_vec.reshaped(X.size(), nv).transpose();
+    Eigen::MatrixXd dtau_dq = dtau_dx.rightCols(nv);
+    Eigen::MatrixXd dtau_dv = dtau_dx.middleCols(nq, nv);
+    Eigen::MatrixXd dtau_da = dtau_dx.middleCols(nq + nv, nv);
+
+    d.Jx_.leftCols(nv) = dtau_dq;
+    d.Jx_.rightCols(nv) = dtau_dv;
+    d.Ju_.leftCols(nv) = dtau_da;
 }
 
-shared_ptr<StageFunctionData> ContactInvDynamicsResidual::createData()
+std::shared_ptr<StageFunctionData> ContactInvDynamicsResidual::createData() const
 {
+    return std::make_shared<ContactInvDynamicsResidualData>(*this);
 }
 
-struct ContactInvDynamicsResidualData : StageFunctionData
+ContactInvDynamicsResidualData::ContactInvDynamicsResidualData(const ContactInvDynamicsResidual &resdl)
+    : StageFunctionData(resdl),
+      tau_(resdl.model_.nv),
+      data_(resdl.model_),
+      dtau_dtaua_(resdl.actuation_matrix_),
+      contact_forces_(4, Vector3d::Zero())
 {
-    ContactInvDynamicsResidualData(const GravityCompensationResidualTpl &resdl);
+    int nu = resdl.actuation_matrix_.cols();
+    tau_.setZero();
+    Ju_.rightCols(nu) = -resdl.actuation_matrix_;
 
-    VectorXd tau_;
-    pinocchio::Data data_;
-    MatrixXd dtau_du_;
-    CppAD::ADFun<double> ad_inv_dynamics_;
-    std::vector<Vector3d> contact_forces_; // 仅仅用于记录数据便于debug
-};
+    //////////// 创建 CppAD 的正动力学函数 //////////
+    using CppAD::AD;
+    using ADVectorX = Eigen::VectorX<AD<double>>;
+
+    pinocchio::ModelTpl<AD<double>> ad_model = resdl.model_.cast<AD<double>>();
+    pinocchio::DataTpl<AD<double>> ad_data(ad_model);
+    int nq = ad_model.nq;
+    int nv = ad_model.nv;
+
+    ADVectorX ad_X(nq + nv + nv + nv); // q, v, a, dq
+    ad_X.setZero();
+    CppAD::Independent(ad_X);
+    ADVectorX ad_Y(nv);
+
+    ADVectorX ad_q = ad_X.head(nq);
+    ADVectorX ad_v = ad_X.segment(nq, nv);
+    ADVectorX ad_a = ad_X.segment(nq + nv, nv);
+    ADVectorX ad_dq = ad_X.tail(nv);
+
+    ADVectorX ad_q_plus = pinocchio::integrate(ad_model, ad_q, ad_dq);
+    pinocchio::forwardKinematics(ad_model, ad_data, ad_q_plus, ad_v);
+    pinocchio::updateFramePlacements(ad_model, ad_data);
+    aligned_vector<pinocchio::ForceTpl<AD<double>>> ad_f_ext(ad_model.njoints,
+                                                             pinocchio::ForceTpl<AD<double>>::Zero());
+    ContactParams<CppAD::AD<double>> ad_contact_params(resdl.contact_params_);
+    CalcContactForceContributionAD(ad_model, ad_data, ad_f_ext, ad_contact_params);
+    ad_Y = pinocchio::rnea(ad_model, ad_data, ad_q_plus, ad_v, ad_a, ad_f_ext);
+    ad_inv_dynamics_.Dependent(ad_X, ad_Y);
+}
